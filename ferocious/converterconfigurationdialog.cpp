d0d62d0e3d8e5f64e7ab0194e64092e754c0ec11
Judd Niemann <jniemann666@bigpond.com>
2018-06-02 23:29:35 +1000

    wherever you go - ther you are

--- ferocious/converterconfigurationdialog.cpp
+++ ferocious/converterconfigurationdialog.cpp
@@ -23,7 +23,6 @@ ConverterConfigurationDialog::ConverterConfigurationDialog(QWidget* parent, Qt::
     // set model
     tableView.setModel(&convertersModel);
     tableView.verticalHeader()->setHidden(true);
-    //tableView.horizontalHeader()->setSectionResizeMode(QHeaderView::ResizeToContents);
 
     // configure widgets
     mainConverterLocationEdit->hideEditButton();
