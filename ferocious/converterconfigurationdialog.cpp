60b4c88043c3344449b3a3f59da9f290ccea67ee
Judd Niemann <jniemann666@bigpond.com>
2018-07-28 20:58:14 +1000

    mmm

--- ferocious/converterconfigurationdialog.cpp
+++ ferocious/converterconfigurationdialog.cpp
@@ -109,7 +109,7 @@ ConverterConfigurationDialog::ConverterConfigurationDialog(QWidget* parent, Qt::
         contextToolBar->raise();
     });
 
-    connect(stdButtons, &QDialogButtonBox::clicked, this, [this, stdButtons](QAbstractButton* b){
+    connect(stdButtons, &QDialogButtonBox::clicked, this, [this, stdButtons](QAbstractButton* b) {
         if(b == stdButtons->button(QDialogButtonBox::RestoreDefaults)){
             onRestoreDefaults();
         }
