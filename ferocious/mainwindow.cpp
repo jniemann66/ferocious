98234371c9ac6aecfa7a60fac808339afc2f220b
jniemann66 <jniemann666@bigpond.com>
2020-02-09 14:56:51 +1100

    added copyright notices

--- ferocious/mainwindow.cpp
+++ ferocious/mainwindow.cpp
@@ -1,3 +1,12 @@
+/*
+* Copyright (C) 2016 - 2020 Judd Niemann - All Rights Reserved.
+* You may use, distribute and modify this code under the
+* terms of the GNU Lesser General Public License, version 2.1
+*
+* You should have received a copy of GNU Lesser General Public License v2.1
+* with this file. If not, please refer to: https://github.com/jniemann66/ferocious
+*/
+
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 #include "outputfileoptions_dialog.h"
