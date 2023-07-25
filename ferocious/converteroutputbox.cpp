df7893d61a01c34cd4c451871e57f43446140a9b
jniemann66 <jniemann666@bigpond.com>
2023-07-25 19:52:48 +1000

    updated copyright notice ;-)

--- ferocious/converteroutputbox.cpp
+++ ferocious/converteroutputbox.cpp
@@ -1,5 +1,5 @@
 /*
-* Copyright (C) 2016 - 2021 Judd Niemann - All Rights Reserved.
+* Copyright (C) 2016 - 2023 Judd Niemann - All Rights Reserved.
 * You may use, distribute and modify this code under the
 * terms of the GNU Lesser General Public License, version 2.1
 *
